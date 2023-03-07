f950ff27cea7e868e289d1f0aa206927926daeaa
Antoine <antoinedorard@gmail.com>
2023-03-07 15:28:51 +0100

    InputProcessor first version done

--- app/src/main/java/backend/CFG_InputProcessor.java
+++ app/src/main/java/backend/CFG_InputProcessor.java
@@ -1,22 +1,22 @@
 package backend;
 
-import jdk.swing.interop.SwingInterOpUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 public class CFG_InputProcessor implements InputProcessor {
 
-    private String file = "./app/TimeTableSkill.json";
-    private ArrayList<JSONArray> slots;
+    private String file = "/skills/TimeTableSkill.json";
+    private ArrayList<JSONArray> slotsValues;
     private ArrayList<String> slotsKeys;
     private HashMap<String, String> placeHolders;
-    private boolean DEBUG = false;
+    private ArrayList<JSONObject> actionObjects;
+    private ArrayList<JSONArray> actionArrays;
+    private String output;
     
     public String processInput(String input) {
         JSONParser parser = new JSONParser(); // create a json parser
@@ -26,60 +26,90 @@ public class CFG_InputProcessor implements InputProcessor {
             JSONObject jsonObject = (JSONObject)obj;
 
 
-            JSONObject subjects = (JSONObject) jsonObject.get("slots");
-            JSONArray dayArray = (JSONArray) subjects.get("DAY");
-            slots = new ArrayList();
-            slotsKeys = new ArrayList<>();
-            placeHolders = new HashMap<>();
-            if(DEBUG)System.out.println(subjects);
+            JSONObject subjects = (JSONObject) jsonObject.get("slots"); //From the JSON Object we select the sltos
+
+            slotsValues = new ArrayList(); //Store the number of subarray of the slots
+            slotsKeys = new ArrayList<>(); //Store the key of the subarray of the slots
+
+            placeHolders = new HashMap<>(); //Store the key words that match the input from the user and the slots values
+
+            actionObjects = new ArrayList<>(); // Store the actions json objects that are relevant to the user input
+            actionArrays = new ArrayList<>();  // Store the actions json arrays that are relevant to the user input
+
             Iterator<String> keysIter = subjects.keySet().iterator();
-
-
-            while(keys.hasNext()) {
-                String key = keys.next();
-                slots.add((JSONArray) subjects.get(key));
+            while(keysIter.hasNext()) { //Store all the keys and values of the slots "DAY : [...]"
+                String key = keysIter.next();
+                slotsKeys.add(key);
+                slotsValues.add((JSONArray) subjects.get(key));
             }
-            for (int i = 0; i < slots.size(); i++) {
+
+            //the for loop is trying to match if words from the input of the user match the words that we have in our slots
+            //O(n*m*p) ~ O(n^3)
+            for (int i = 0; i < slotsValues.size(); i++) {
                 to:
                 for (int j = 0; j < words.length; j++) {
-                    for (int k = 0; k < (slots.get(i)).size(); k++) {
-                        //System.out.println(i + " " + j + " " + k + " ");
-                        if(words[j].equals((slots.get(i)).get(k))){
+                    for (int k = 0; k < (slotsValues.get(i)).size(); k++) {
+                        if(words[j].equals((slotsValues.get(i)).get(k))){
                             placeHolders.put(slotsKeys.get(i), words[j]);
-                            if(DEBUG)System.out.println("this is a keyword " + words[j]);
-                            if(DEBUG)System.out.println(slots.get(i).size());
                             break to;
                         }
                     }
                 }
             }
-            JSONObject actions = (JSONObject) jsonObject.get("actions");
-            //System.out.println(((JSONObject)actions.get(placeHolders.get(1))).get(placeHolders.get(0)));
-//            for (Map.Entry placeholder : placeHolders.entrySet()) {
-//                JSONObject temp = (JSONObject) actions.get(placeholder.getValue());
-//                actions = temp;
-//
-//                System.out.println("this si loop " + actions);
-//            }
-
-            System.out.println("Placeholders:");
-            System.out.println(placeHolders.get("DAY") + "  " + placeHolders.get("TIME"));
-            System.out.println("Actions:");
-            System.out.println(actions);
-
-            // to associate the placeholder with all the key
-            /*for (Map.Entry placeholder : placeHolders.entrySet()) {
-                for (Map.Entry action : actions.entrySet()){
-
+            JSONObject actions = (JSONObject) jsonObject.get("actions"); //get the JSONObject of the actions
+            //loop through placeholders
+            // and get all the keys to points to null
+            for (String key : slotsKeys) {
+                if (placeHolders.get(key) == null) {
+                    return "I don't understand, please try again (You might be missing a keyword)";
                 }
-            }*/
+            }
 
+            traverse(actions);
+            return "You have "+ output+ " class at " + placeHolders.get("TIME") + " o'clock on " + placeHolders.get("DAY");
 
 
         }
         catch(Exception IO){
-            System.out.println("There is a problem with the code");
+            System.err.println("Error in CFG_InputProcessor.java");
+            System.out.println(IO.getMessage());
         }
-        return null;
+
+        return "Error in CFG_InputProcessor";
+    }
+
+
+    public void traverse(Object obj) {
+        if (obj instanceof JSONObject) {
+            JSONObject jsonObj = (JSONObject) obj;
+            for (Object key : jsonObj.keySet()) {
+
+                String[] keySplit = ((String) key).split("\\s+");
+
+                if(placeHolders.containsKey(keySplit[0])){
+                    System.out.println("this is a placeholder " + placeHolders.get(keySplit[0]));
+
+                    if(placeHolders.get(keySplit[0]).equals(keySplit[1])){
+                        traverse(jsonObj.get(key));
+                        System.out.println("this is a match " + jsonObj.get(key));
+                        if(jsonObj.get(key) instanceof JSONObject)  actionObjects.add((JSONObject) jsonObj.get(key));
+                        else if(jsonObj.get(key) instanceof JSONArray) actionArrays.add((JSONArray) jsonObj.get(key));
+                        else {
+                            output = (String) jsonObj.get(key);
+                            return;
+                        }
+                    }
+                }
+            }
+        } else if (obj instanceof JSONArray) {
+            JSONArray jsonArr = (JSONArray) obj;
+            for (Object elem : jsonArr) {
+                // TODO in case of arrays in the actions
+                traverse(elem);
+            }
+        } else {
+            //System.out.println(obj.toString());
+            //TODO maybe going to use it later
+        }
     }
 }
