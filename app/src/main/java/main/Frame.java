f8ec0c93062b19fffef28e783c0b3fa2f954e6b6
StefanCals <36990897+StefanCals@users.noreply.github.com>
2023-03-07 17:10:48 +0100

    Navigation frontend done
    - added navigation for switching between panels.
    - added new skillEditorPanel
    - added controls & funtionality to skillEditorPanel

--- app/src/main/java/main/Frame.java
+++ app/src/main/java/main/Frame.java
@@ -2,37 +2,73 @@ package main;
 
 import panels.ChatBotPanel;
 import panels.SelectOptionsPanel;
+import panels.SkillEditorPanel;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Frame extends JFrame {
 
     final int winWidth = 850;
     final int winHeight = 600;
-    JPanel chatBotPanel, selectOptionsPanel;
+    JPanel mainPanel;
+    SelectOptionsPanel selectOptionsPanel;
+    ChatBotPanel chatBotPanel;
+    SkillEditorPanel skillEditorPanel;
+    CardLayout cL;
+    JPanel cards;
 
     public Frame(){
         super("UM DACS Project 2-2 - Group 07 Chatbot");
 
         this.setLayout(new BorderLayout(10,0));
+
+        // create the panel that contains the navigation buttons.
         selectOptionsPanel = new SelectOptionsPanel();
         selectOptionsPanel.setPreferredSize(new Dimension(100, 480));
         this.add(selectOptionsPanel, BorderLayout.LINE_START);
+
+        // create your sub-panels.
         chatBotPanel = new ChatBotPanel();
-        this.add(chatBotPanel, BorderLayout.CENTER);
+        skillEditorPanel = new SkillEditorPanel();
 
+        // create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(chatBotPanel, "ChatBotPanel");
+        cards.add(skillEditorPanel, "SkillsEditorPanel");
 
+        // get the card-layout, which was created as a layout around the "cards".
+        cL = (CardLayout)(cards.getLayout());
+
+        // default show the first panel.
+        cL.show(cards, "ChatBotPanel");
+
+        // add the cards to this Frame.
+        this.add(cards);
+
+        // set some Frame variables.
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setSize(winWidth, winHeight);
         this.setLocationRelativeTo(null);
         this.setIconImage(new ImageIcon("app/src/imgs/UMlogo.jpg").getImage());
-
         this.setVisible(true);
+
+        // define the navigation button Listeners.
+        for (JToggleButton btn :  selectOptionsPanel.naviButtons) {
+            btn.addActionListener(e -> {
+                actionPerformed(e.getActionCommand());
+            });
+        }
+    }
+
+    public void actionPerformed(String buttonName) // define your Listener action.
+    {
+        if (buttonName == "Chat"){
+            cL.show(cards, "ChatBotPanel");
+        }
+        else if (buttonName == "Skills"){
+            cL.show(cards, "SkillsEditorPanel");
+        }
     }
 }
