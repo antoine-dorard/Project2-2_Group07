485ed603c53884df4c6717d6c505a15c17c1ade1
Alexandre Martens <alexandre.martens@outlook.com>
2023-03-15 11:37:25 +0100

    Fixed and optimised ze code
    Fix the doc read, Speed optimisations and used more efficient data structures. Brrr

--- app/src/main/java/Spelling_Checker/WordSuggester.java
+++ app/src/main/java/Spelling_Checker/WordSuggester.java
@@ -5,58 +5,61 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-public class WordSuggester{
-    String file = "Spelling_Checker/words.txt";
-    ArrayList<String> wordlist = new ArrayList<String>();
-    String input = "hallo";
+public class WordSuggester {
+    String file = "app/src/main/java/Spelling_Checker/words.txt";
+    ArrayList<String> wordlist = new ArrayList<>();
     LevenshteinDistance lDistance = new LevenshteinDistance();
-    int i;
-    HashMap<String, Integer> newlist = new HashMap<String, Integer>();
 
-    public void inputMatches(String input){
-        try{
-            loadworddoc(file);
+    // Added error handling
+    public void inputMatches(String input) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.length() > 2 && line.length() < 26) {
+                    wordlist.add(line);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading word document: " + e.getMessage());
+            return;
         }
-        catch (IOException e) {
-            System.out.println("didn't find the file");
-            e.printStackTrace();
-        }
-        for(String word : wordlist){
-            i = lDistance.computeDistance(word,input);
-            if(i<3){
-                newlist.put(word,i);
+        
+        // The HashMap<String, Integer> newlist got replaced with Map<String, List<Integer>> newlist = new HashMap<>().
+        // The reason for this change is that the original HashMap only stored one integer value per word,
+        // which meant that if we had multiple words with the same distance to the input word,
+        // only one of them would be stored. 
+        // By using a HashMap with a List, multiple distances can be stored for each word,
+        // this is more efficient for the sorting of the distances and returning more.
+        System.out.println("Word\tDistance");
+        Map<String, List<Integer>> newlist = new HashMap<>();
+        for (String word : wordlist) {
+            int i = lDistance.computeDistance(word, input);
+            System.out.printf("%s\t%d\n", word, i);
+
+            if (i < 3) {
+                newlist.computeIfAbsent(word, k -> new ArrayList<>()).add(i);
             }
         }
-        // Sorting to get the best cities in the top
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(newlist.entrySet());
 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
+        System.out.println("newlist: " + newlist);
 
-        LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> entry : list) {
+        List<Map.Entry<String, List<Integer>>> list = new ArrayList<>(newlist.entrySet());
+
+        // I simplified the sorting logic to use the Collections.min() method to find
+        // the minimum distance for each word, rather than sorting the list.
+        Collections.sort(list, Comparator.comparing(entry -> Collections.min(entry.getValue())));
+
+        LinkedHashMap<String, List<Integer>> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, List<Integer>> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
 
-        System.out.println(sortedMap);
-    }
-
-
-    public void loadworddoc(String filename) throws IOException {
-        String line;
-        BufferedReader br = new BufferedReader(new FileReader(filename));
-        while((line = br.readLine()) != null){
-            if(line.length()>2 && line.length()<26)
-                wordlist.add(line);
-        }
+        System.out.println("sortedMap: " + sortedMap);
     }
 
     public static void main(String[] args) {
         WordSuggester ws = new WordSuggester();
-        String input = "hella";
+        String input = "hello";
         ws.inputMatches(input);
     }
 }
