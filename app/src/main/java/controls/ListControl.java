bac64d8379148468ceb738fe32ab837856e2e8f4
StefanCals <36990897+StefanCals@users.noreply.github.com>
2023-03-20 10:55:42 +0100

    Added ListControl as seperate class

---
+++ app/src/main/java/controls/ListControl.java
@@ -0,0 +1,47 @@
+package controls;
+
+import utils.ConfigUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ListControl extends JList<String> {
+
+    private final ConfigUI configUI = new ConfigUI();
+
+
+    public ListControl(){
+        super();
+
+        // define a list model, which will work as skill selector.
+        DefaultListModel<String> listModel = new DefaultListModel<>();
+
+        // set this model to the JList (ListControl).
+        setModel(listModel);
+
+        // define the list items and call the setup method for the JList.
+        String[] listItems = {"Math", "History", "Calendar", "etc"};
+
+        // add all items, provided in the input, to the current JList.
+        for (String item :  listItems) {listModel.addElement(item);}
+        System.out.println("added");
+
+        // define the list appearance and functionality.
+        setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        setLayoutOrientation(JList.VERTICAL);
+        setVisibleRowCount(-1);
+        setBackground(configUI.colorListBG);
+        setForeground(configUI.colorListFG);
+        setFont(configUI.fontList);
+        setSelectionBackground(configUI.colorListSelectionBG);
+        setSelectionForeground(configUI.colorListSelectionFG);
+        setPreferredSize(new Dimension(200, 550));
+
+        // define the list scroller for the list control.
+        JScrollPane listScroller = new JScrollPane(this);
+        listScroller.setPreferredSize(new Dimension(250, 80));
+
+    }
+
+}
+
--- app/src/main/java/panels/SkillEditorPanel.java
+++ app/src/main/java/panels/SkillEditorPanel.java
@@ -2,6 +2,7 @@ package panels;
 
 import utils.ConfigUI;
 import controls.TreeControl;
+import controls.ListControl;
 
 import javax.swing.*;
 import javax.swing.event.*;
@@ -32,80 +33,20 @@ public class SkillEditorPanel extends JPanel{
 
     public SkillEditorPanel(){
         super();
+        // define the main layout of this panel.
         FlowLayout layout = new FlowLayout();
         layout.setAlignment(FlowLayout.LEFT);
         setLayout(layout);
-        //JPanel listPane = new JPanel();
-        //JPanel treePane = new JPanel();
-        //add(listPane);
-        //add(treePane);
 
-        //this.setLayout(layout);
-        //this.setBackground(configUI.colorPanelBG);
-
-
-        // define a boxLayout for all input JTextFields.
-        JPanel boxPane = new JPanel();
-        boxPane.setLayout(new BoxLayout(boxPane, BoxLayout.PAGE_AXIS));
-        boxPane.setPreferredSize(new Dimension(400, 500));
-        boxPane.setOpaque(false);
-
-        // define a list model, which will work as skill selector.
-        DefaultListModel<String> listModel = new DefaultListModel<>();
-
-        // define the list control with the list model created earlier.
-        JList<String> skillList = new JList<>(listModel);
-
-        // define the list items and call the setup method for the JList.
-        String[] listItems = {"Math", "History", "Calendar", "etc"};
-        setupJList(skillList,listItems);
-
+        ListControl skillList = new ListControl();
         TreeControl treeControl = new TreeControl();
-
         JTextField treeTextField = new JTextField();
         setupJTextField(treeTextField, 400, 40);
 
-        // create all JTextFields.
-        JTextField skillName = new JTextField();
-        JTextField skillRule = new JTextField();
-        JTextField skillIdentifier = new JTextField();
-
         // create all JLabels.
-        JLabel nameLabel = new JLabel();
-        JLabel ruleLabel = new JLabel();
-        JLabel identifierLabel = new JLabel();
+        //JLabel nameLabel = new JLabel();
+        //setupJLabel(nameLabel, "Name", 250, 40);
 
-        setupJLabel(nameLabel, "Name", 250, 40);
-        setupJLabel(ruleLabel, "Rule(s)", 250, 80);
-        setupJLabel(identifierLabel, "Identifier(s)", 250, 80);
-
-        // setup all JTextFields.
-        setupJTextField(skillName, 250, 20);
-        setupJTextField(skillRule, 250, 80);
-        setupJTextField(skillIdentifier, 250, 80);
-
-        // add all the JTextFields to the JPanel.
-        boxPane.add(nameLabel);
-        boxPane.add(treeControl);
-        //boxPane.add(Box.createRigidArea(new Dimension(0, 10)));
-        //boxPane.add(skillName);
-        //boxPane.add(ruleLabel);
-        //boxPane.add(Box.createRigidArea(new Dimension(0, 10)));
-        //boxPane.add(skillRule);
-        //boxPane.add(identifierLabel);
-        //boxPane.add(Box.createRigidArea(new Dimension(0, 10)));
-        //boxPane.add(skillIdentifier);
-
-        // add the skill selector to this JPanel. (do this first)
-        //add(skillList);
-        // add the boxPane, which contains all JTextFields.
-        //this.add(boxPane);
-        //treeControl.setAlignmentY(JTree.TOP_ALIGNMENT);
-        //add(treeControl);
-        //setAlignmentY(JPanel.TOP_ALIGNMENT);
-        //add(skillList, BorderLayout.NORTH);
-        //skillList.setPreferredSize(new Dimension(100, 200));
-        //add(treeControl, BorderLayout.NORTH);
         skillList.setPreferredSize(new Dimension(200, 600));
         treeControl.setPreferredSize(new Dimension(200, 600));
         add(skillList);
@@ -119,12 +60,6 @@ public class SkillEditorPanel extends JPanel{
         // add Action Listener for the JList "skillList".
         skillList.addListSelectionListener(new ListSelectionListener() {
             public void valueChanged(ListSelectionEvent evt) {
-                skillListValueChanged(evt);
-            }
-            private void skillListValueChanged(ListSelectionEvent evt) {
-                if (!skillList.getValueIsAdjusting()) {
-                    skillName.setText((String) skillList.getSelectedValue());
-                }
             }
         });
 
@@ -162,30 +97,6 @@ public class SkillEditorPanel extends JPanel{
         DefaultMutableTreeNode node = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
         node.setUserObject(textField.getText());
         model.nodeChanged(node);
-    }
-
-    private void setupJList (JList<String> list, String[] listItems){
-
-        // retrieve listModel and set all items from the input array.
-        DefaultListModel<String> model = (DefaultListModel<String>) list.getModel();
-
-        // add all items, provided in the input, to the current JList.
-        for (String item :  listItems) {model.addElement(item);}
-
-        // define the list appearance and functionality.
-        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        list.setLayoutOrientation(JList.VERTICAL);
-        list.setVisibleRowCount(-1);
-        list.setBackground(configUI.colorListBG);
-        list.setForeground(configUI.colorListFG);
-        list.setFont(configUI.fontList);
-        list.setSelectionBackground(configUI.colorListSelectionBG);
-        list.setSelectionForeground(configUI.colorListSelectionFG);
-        list.setPreferredSize(new Dimension(200, 550));
-
-        // define the list scroller for the list control.
-        JScrollPane listScroller = new JScrollPane(list);
-        listScroller.setPreferredSize(new Dimension(250, 80));
     }
 
     private void setupJTextField (JTextField textField, int width, int height) {
